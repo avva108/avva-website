a910b2c35ab918867e62d33156ab4b8fe1851f47
unknown <abhinav.90444@gmail.com>
2021-10-30 01:06:13 +0530

    adio player starting

---
+++ raman/main.go
@@ -0,0 +1,5 @@
+package main
+fun main(){
+	println9("helloworld")
+
+}
---
+++ raman/player.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {startplayer();},false );
+var player;
+function startplayer()
+{
+    player = document.getElementById('music_player');
+    player.controls = false;
+}
+
+function play_aud()
+{
+    player.play();
+}
+function pause_aud()
+{
+    player.pause();
+}
+function stop_aud()
+{
+    player.pause();
+    player.currentTime = 0;
+}
+function change_vol()
+{
+    player.volume=document.getElementById("change_vol").value;
+}
